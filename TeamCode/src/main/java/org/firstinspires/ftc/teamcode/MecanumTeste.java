1e81d66c55e6a7a54d8a0fe16f838dd670acfacd
jeniusgregic <114189054+jeniusgregic@users.noreply.github.com>
2025-09-10 17:29:38 -0400

    first commit for nightly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeste.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeste.java
@@ -3,31 +3,64 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp (name = "OpModeMecanum")
 
 public class MecanumTeste extends LinearOpMode {
 
+    float x,y,turn;
+
+    double denominator;
+
+    double leftFrontPower, leftBackPower,rightFrontPower,rightBackPower;
+
     DcMotor leftFront;
     DcMotor rightFront;
     DcMotor leftBack;
     DcMotor rightBack;
 
-    public void runOpMode() throws InterruptedException {
-        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
-        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+    public void runOpMode() {
 
-        waitForStart(); // espera o start no DS
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+        leftBack.setDirection(DcMotorEx.Direction.REVERSE);
+        leftFront.setDirection(DcMotorEx.Direction.FORWARD);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
 
         while (opModeIsActive()) {
-            if (gamepad1.a) {
-                leftFront.setPower(1);
-            } else {
-                leftFront.setPower(0);
+
+            x = gamepad1.left_stick_x;
+            y = gamepad1.left_stick_y;
+            turn = - gamepad1.right_stick_x;
+
+            denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(turn), 1);
+
+            leftFrontPower = ((y + x + turn) / denominator);
+            leftBackPower = ((y - x + turn) / denominator);
+            rightFrontPower  = ((y - x - turn) / denominator);
+            rightBackPower = ((y + x - turn) / denominator);
+
+            leftFront.setPower(leftFrontPower);
+            leftBack.setPower(leftBackPower);
+            rightFront.setPower(rightFrontPower);
+            rightBack.setPower(rightBackPower);
+
+
+
             }
         }
     }
 
 
-}
+
