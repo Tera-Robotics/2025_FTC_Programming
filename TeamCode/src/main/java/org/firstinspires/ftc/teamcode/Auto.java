f103b4c47926c6e905453b6e32754ac006ea6c7e
Tera-Robotics <terarobotics.programacao@gmail.com>
2025-11-27 19:16:15 -0400

    6+3 autonomo semi perfect, and auto functions separeted of other pathing´s. NEED RED SIDE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,79 +1,70 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-import org.firstinspires.ftc.teamcode.states.Intake;
-import org.firstinspires.ftc.teamcode.states.Shooter;
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
-@Autonomous(name = "First Auto", group = "Autonomous")
+public class Auto {
 
-public class Auto extends LinearOpMode {
-private DcMotor leftFront, leftBack, rightFront,rightBack;
+    public DcMotor leftFront, leftBack, rightFront, rightBack;
+    public IMU imu;
 
-private IMU imu;
-
+    public LinearOpMode opMode;
 
     private double headingError = 0;
-    private double  targetHeading = 0;
-    private double  turnSpeed     = 1;
-    private int     leftFrontTarget    = 0;
+    private double targetHeading = 0;
+    private double turnSpeed = 1;
 
-    private int     leftBackTarget    = 0;
+    private int leftFrontTarget = 0;
+    private int leftBackTarget = 0;
+    private int rightFrontTarget = 0;
+    private int rightBackTarget = 0;
 
-    private int     rightFrontTarget    = 0;
-    private int     rightBackTarget   = 0;
     static final double COUNTS_PER_MOTOR_REV = 318;
-    static final double WHELL_DIAMETER_CM = 9.6;
-
-    static final double COUNTS_PER_CM = COUNTS_PER_MOTOR_REV / (WHELL_DIAMETER_CM*Math.PI);
-
-    static final double TURN_SPEED = 1;
+    static final double WHEEL_DIAMETER_CM = 9.6;
+    static final double COUNTS_PER_CM = COUNTS_PER_MOTOR_REV / (WHEEL_DIAMETER_CM * Math.PI);
 
     static final int DRIVE_SPEED = 1;
+    static final double HEADING_THRESHOLD = 2;
+    static final double P_TURN_GAIN = 0.01;
+    static final double P_DRIVE_GAIN = 0.003;
 
-    static final double STRAFE_SPEED = 0.6;
+    public Auto(HardwareMap hardwareMap, LinearOpMode opMode) {
 
-    static final double     HEADING_THRESHOLD       = 2;
+        this.opMode = opMode;
 
-    static final double     P_TURN_GAIN            = 0.01;
-    static final double     P_DRIVE_GAIN           = 0.003;
-
-    Intake intake = null;
-    Shooter shooter = null;
-
-
-
-    @Override
-    public void runOpMode() {
-        
         leftFront  = hardwareMap.get(DcMotor.class, "leftFront");
-        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
-        rightFront  = hardwareMap.get(DcMotor.class, "rightFront");
-        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
-
-        intake = new Intake(hardwareMap);
-        shooter = new Shooter(hardwareMap);
+        leftBack   = hardwareMap.get(DcMotor.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightBack  = hardwareMap.get(DcMotor.class, "rightBack");
 
         leftBack.setDirection(DcMotorEx.Direction.REVERSE);
         leftFront.setDirection(DcMotorEx.Direction.REVERSE);
         rightFront.setDirection(DcMotorEx.Direction.FORWARD);
         rightBack.setDirection(DcMotorEx.Direction.FORWARD);
-        
-        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
-        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
-        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection =
+                RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        RevHubOrientationOnRobot.UsbFacingDirection usbDirection =
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
 
         imu = hardwareMap.get(IMU.class, "imu");
-        imu.initialize(new IMU.Parameters(orientationOnRobot));
+        imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(logoDirection, usbDirection)));
 
+        resetEncoders();
+
+        imu.resetYaw();
+    }
+
+    private void resetEncoders() {
         leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -84,85 +75,26 @@ private IMU imu;
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-
-        while (opModeInInit()) {
-            telemetry.addData(">", "Robot Heading = %4.0f", getHeading());
-            telemetry.update();
-        }
-
         leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        imu.resetYaw();
-
-        // Primeiro Ciclo de shooting
-        shooter.moveToDefault();
-        sleep(1000);
-        shooter.moveToShoot();
-        sleep(1000);
-
-        //Segundo ciclo de shooting
-        shooter.stop();
-        driveStraight(0.6,70,0,0.6);
-        turnToHeading(TURN_SPEED,40,1);
-        strafe(STRAFE_SPEED,80,0.6);
-        intake.starCollectBall();
-        shooter.moveToDefault();
-        driveStraight(0.3,-70,0,0.3);
-        shooter.stop();
-        sleep(800);
-        intake.stopCollectBall();
-        driveStraight(0.2,30,0,0.2);
-        strafe(0.6,-120,1);
-        turnToHeading(TURN_SPEED,1,0.8);
-        driveStraight(1,-30,0,0.8);
-        shooter.moveToDefault();
-        sleep(1000);
-        shooter.moveToShoot();
-        sleep(1000);
-        shooter.stop();
-
-        //Terceiro ciclo de shooting
-        driveStraight(0.6,70,0,0.6);
-        turnToHeading(TURN_SPEED,41,1);
-        strafe(STRAFE_SPEED,165,0.7);
-        intake.starCollectBall();
-        shooter.moveToDefault();
-        driveStraight(0.2,-80,0,0.2);
-        shooter.stop();
-        sleep(800);
-        intake.stopCollectBall();
-        driveStraight(0.2,20,0,0.2);
-        strafe(0.6,-180,1);
-        turnToHeading(TURN_SPEED,2,1);
-        driveStraight(1,-30,0,0.8);
-        shooter.moveToDefault();
-        sleep(1000);
-        shooter.moveToShoot();
-        sleep(1000);
-        shooter.stop();
-        strafe(0.6,180,0.8);
-
-
-        telemetry.addData("Path", "Complete");
-        telemetry.update();
-        sleep(1000);  // Pause to display last telemetry message.
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
+    private boolean opModeIsActive() {
+        return opMode.opModeIsActive();
+    }
 
-    public void driveStraight(double maxDriveSpeed,
-                              double distance,
-                              double heading,
-                              double SPEED_STRAIGHT) {
+    public void driveStraight(double maxDriveSpeed, double distance, double heading, double SPEED_STRAIGHT) {
+
         if (opModeIsActive()) {
 
-            // Determine new target position, and pass to motor controller
             int moveCounts = (int)(distance * COUNTS_PER_CM);
-            leftFrontTarget = leftFront.getCurrentPosition() + moveCounts;
-            leftBackTarget = leftBack.getCurrentPosition() + moveCounts;
+
+            leftFrontTarget  = leftFront.getCurrentPosition()  + moveCounts;
+            leftBackTarget   = leftBack.getCurrentPosition()   + moveCounts;
             rightFrontTarget = rightFront.getCurrentPosition() + moveCounts;
-            rightBackTarget = rightBack.getCurrentPosition() + moveCounts;
+            rightBackTarget  = rightBack.getCurrentPosition()  + moveCounts;
 
             leftFront.setTargetPosition(leftFrontTarget);
             leftBack.setTargetPosition(leftBackTarget);
@@ -170,128 +102,56 @@ private IMU imu;
             rightBack.setTargetPosition(rightBackTarget);
 
             leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-            maxDriveSpeed = Math.abs(maxDriveSpeed);
-            moveRobot(maxDriveSpeed, 0,0,SPEED_STRAIGHT);
-
+            moveRobot(maxDriveSpeed, 0, 0, SPEED_STRAIGHT);
 
             while (opModeIsActive() &&
-                    (leftFront.isBusy() && rightFront.isBusy() && leftBack.isBusy() && rightBack.isBusy())) {
+                    leftFront.isBusy() && leftBack.isBusy() &&
+                    rightFront.isBusy() && rightBack.isBusy()) {
 
                 turnSpeed = getSteeringCorrection(heading, P_DRIVE_GAIN);
 
-                // if driving in reverse, the motor correction also needs to be reversed
-                if (distance < 0)
-                    turnSpeed *= -1.0;
+                if (distance < 0) turnSpeed *= -1;
 
-                moveRobot(DRIVE_SPEED, 0,turnSpeed,SPEED_STRAIGHT);
-
-                sendTelemetry(true);
+                moveRobot(DRIVE_SPEED, 0, turnSpeed, SPEED_STRAIGHT);
+                //sendTelemetry(true);
             }
 
-            moveRobot(0, 0,0,0);
-
-            leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+            moveRobot(0,0,0,0);
+            resetEncoders();
         }
     }
 
-    public void turnToHeading(double maxTurnSpeed, double heading,double TURN_SPEED) {
+    public void turnToHeading(double maxTurnSpeed, double heading, double TURN_SPEED) {
 
         while (opModeIsActive()) {
 
             headingError = heading - getHeading();
-
             while (headingError > 180) headingError -= 360;
             while (headingError <= -180) headingError += 360;
 
-            // Agora sim está correto
             if (Math.abs(headingError) <= HEADING_THRESHOLD)
                 break;
 
             turnSpeed = getSteeringCorrection(heading, P_TURN_GAIN);
             turnSpeed = Range.clip(turnSpeed, -maxTurnSpeed, maxTurnSpeed);
 
-            moveRobot(0, 0, turnSpeed,TURN_SPEED);
-
-            sendTelemetry(false);
+            moveRobot(0, 0, turnSpeed, TURN_SPEED);
+            //sendTelemetry(false);
         }
 
-        moveRobot(0, 0, 0,0);
+        moveRobot(0, 0, 0, 0);
     }
 
-    public void holdHeading(double maxTurnSpeed, double heading, double holdTime) {
+    public void strafe(double speed, double distanceCm, double STRAFE) {
 
-        ElapsedTime holdTimer = new ElapsedTime();
-        holdTimer.reset();
-
-        // keep looping while we have time remaining.
-        while (opModeIsActive() && (holdTimer.time() < holdTime)) {
-            // Determine required steering to keep on heading
-            turnSpeed = getSteeringCorrection(heading, P_TURN_GAIN);
-
-            // Clip the speed to the maximum permitted value.
-            turnSpeed = Range.clip(turnSpeed, -maxTurnSpeed, maxTurnSpeed);
-
-            // Pivot in place by applying the turning correction
-            moveRobot(0, turnSpeed,0,0);
-
-            // Display drive status for the driver.
-            sendTelemetry(false);
-        }
-
-        // Stop all motion;
-        moveRobot(0, 0,0,0);
-    }
-
-    public double getSteeringCorrection(double desiredHeading, double proportionalGain) {
-        targetHeading = desiredHeading;  // Save for telemetry
-
-        // Determine the heading current error
-        headingError = targetHeading - getHeading();
-
-        // Normalize the error to be within +/- 180 degrees
-        while (headingError > 180)  headingError -= 360;
-        while (headingError <= -180) headingError += 360;
-
-        // Multiply the error by the gain to determine the required steering correction/  Limit the result to +/- 1.0
-        return Range.clip(headingError * proportionalGain, -1, 1);
-    }
-
-    public void moveRobot(double forward, double strafe, double rotate, double speed) {
-
-        double leftFrontPower  = forward + strafe + rotate;
-        double rightFrontPower = forward - strafe - rotate;
-        double leftBackPower   = forward - strafe + rotate;
-        double rightBackPower  = forward + strafe - rotate;
-
-        double maxPower = 1;
-        double maxSpeed = speed;
-
-        maxPower = Math.max(maxPower, Math.abs(leftFrontPower));
-        maxPower = Math.max(maxPower, Math.abs(leftBackPower));
-        maxPower = Math.max(maxPower, Math.abs(rightFrontPower));
-        maxPower = Math.max(maxPower, Math.abs(rightBackPower));
-
-        leftFront.setPower(maxSpeed * (leftFrontPower / maxPower));
-        leftBack.setPower(maxSpeed * (leftBackPower / maxPower));
-        rightFront.setPower(maxSpeed * (rightFrontPower / maxPower));
-        rightBack.setPower(maxSpeed * (rightBackPower / maxPower));
-    }
-
-    public void strafe(double speed, double distanceCm,double STRAFE) {
         if (!opModeIsActive()) return;
 
         int moveCounts = (int)(distanceCm * COUNTS_PER_CM);
 
-        // Para strafe à direita: motores mecanum se movem assim:
-        // LF: +, LB: -, RF: -, RB: +
         leftFrontTarget  = leftFront.getCurrentPosition()  + moveCounts;
         leftBackTarget   = leftBack.getCurrentPosition()   - moveCounts;
         rightFrontTarget = rightFront.getCurrentPosition() - moveCounts;
@@ -307,44 +167,66 @@ private IMU imu;
         rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        speed = Math.abs(speed);
-        moveRobot(0, speed, 0,STRAFE);  // strafe positivo = para direita
+        moveRobot(0, speed, 0, STRAFE);
 
         while (opModeIsActive() &&
-                (leftFront.isBusy() && rightFront.isBusy() &&
-                        leftBack.isBusy() && rightBack.isBusy())) {
-            sendTelemetry(true);
+                leftFront.isBusy() && rightFront.isBusy() &&
+                leftBack.isBusy() && rightBack.isBusy()) {
+
+            //sendTelemetry(true);
         }
 
-        moveRobot(0, 0, 0,0);
-
-        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        moveRobot(0,0,0,0);
+        resetEncoders();
     }
 
+    public void moveRobot(double forward, double strafe, double rotate, double speed) {
 
+        double leftFrontPower  = forward + strafe + rotate;
+        double rightFrontPower = forward - strafe - rotate;
+        double leftBackPower   = forward - strafe + rotate;
+        double rightBackPower  = forward + strafe - rotate;
 
+        double maxPower = Math.max(1.0,
+                Math.max(Math.abs(leftFrontPower),
+                        Math.max(Math.abs(rightFrontPower),
+                                Math.max(Math.abs(leftBackPower), Math.abs(rightBackPower)))));
 
-    private void sendTelemetry(boolean straight) {
+        leftFront.setPower(speed * (leftFrontPower / maxPower));
+        leftBack.setPower(speed * (leftBackPower / maxPower));
+        rightFront.setPower(speed * (rightFrontPower / maxPower));
+        rightBack.setPower(speed * (rightBackPower / maxPower));
+    }
 
-        if (straight) {
-            telemetry.addData("Motion", "Drive Straight");
-            telemetry.addData("Target Pos L:R",  "%7d:%7d", leftBackTarget, rightBackTarget,leftFrontTarget,rightFrontTarget);
-            telemetry.addData("Actual Pos L:R",  "%7d:%7d",      leftFront.getCurrentPosition(),
-                    rightFront.getCurrentPosition());
-        } else {
-            telemetry.addData("Motion", "Turning");
-        }
+    public double getSteeringCorrection(double desiredHeading, double proportionalGain) {
 
-        telemetry.addData("Heading- Target : Current", "%5.2f : %5.0f", targetHeading, getHeading());
-        telemetry.addData("Error  : Steer Pwr",  "%5.1f : %5.1f", headingError, turnSpeed);
-        telemetry.update();
+        targetHeading = desiredHeading;
+
+        headingError = targetHeading - getHeading();
+        while (headingError > 180) headingError -= 360;
+        while (headingError <= -180) headingError += 360;
+
+        return Range.clip(headingError * proportionalGain, -1, 1);
     }
 
     public double getHeading() {
-        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-        return orientation.getYaw(AngleUnit.DEGREES);
+        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
     }
+
+    /*public void sendTelemetry(boolean straight) {
+
+        if (straight) {
+            opMode.telemetry.addData("Motion", "Drive Straight");
+            opMode.telemetry.addData("Target Pos", "%7d | %7d", leftFrontTarget, rightFrontTarget);
+            opMode.telemetry.addData("Actual Pos", "%7d | %7d",
+                    leftFront.getCurrentPosition(), rightFront.getCurrentPosition());
+        } else {
+            opMode.telemetry.addData("Motion", "Turning");
+        }
+
+        opMode.telemetry.addData("Heading Target : Current", "%5.1f : %5.1f",
+                targetHeading, getHeading());
+        opMode.telemetry.addData("Error : Steer Pwr", "%5.1f : %5.1f", headingError, turnSpeed);
+        opMode.telemetry.update();
+    }*/
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pathing/RedAllience1.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode.Pathing;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.Auto;
+import org.firstinspires.ftc.teamcode.states.Intake;
+import org.firstinspires.ftc.teamcode.states.Shooter;
+
+@Autonomous(name = "Red 1", group = "Autonomous")
+public class RedAllience1 extends LinearOpMode {
+
+    static final double TURN_SPEED = 1;
+
+    static final double STRAFE_SPEED = 0.6;
+
+    Intake intake = null;
+    Shooter shooter = null;
+
+    Auto auto = null;
+
+    @Override
+    public void runOpMode() {
+
+        intake = new Intake(hardwareMap);
+        shooter = new Shooter(hardwareMap);
+        auto = new Auto(hardwareMap,this);
+
+        waitForStart();
+
+        // Primeiro Ciclo de shooting
+        shooter.moveToDefault();
+        sleep(1000);
+        shooter.moveToShoot();
+        sleep(1000);
+
+        //Segundo ciclo de shooting
+        shooter.stop();
+        auto.driveStraight(0.6, 70, 0, 0.6);
+        auto.turnToHeading(TURN_SPEED, 40, 1);
+        auto.strafe(STRAFE_SPEED, 80, 0.6);
+        intake.starCollectBall();
+        shooter.moveToDefault();
+        auto.driveStraight(0.3, -70, 0, 0.3);
+        shooter.stop();
+        sleep(800);
+        intake.stopCollectBall();
+        auto.driveStraight(0.2, 30, 0, 0.2);
+        auto.strafe(0.6, -120, 1);
+        auto.turnToHeading(TURN_SPEED, 1, 0.8);
+        auto.driveStraight(1, -30, 0, 0.8);
+        shooter.moveToDefault();
+        sleep(1000);
+        shooter.moveToShoot();
+        sleep(1000);
+        shooter.stop();
+
+        //Terceiro ciclo de shooting
+        auto.driveStraight(0.6, 70, 0, 0.6);
+        auto.turnToHeading(TURN_SPEED, 41, 1);
+        auto.strafe(STRAFE_SPEED, 165, 0.7);
+        intake.starCollectBall();
+        shooter.moveToDefault();
+        auto.driveStraight(0.2, -80, 0, 0.2);
+        shooter.stop();
+        sleep(800);
+        intake.stopCollectBall();
+        auto.driveStraight(0.2, 20, 0, 0.2);
+        auto.strafe(0.6, -180, 1);
+        auto.turnToHeading(TURN_SPEED, 2, 1);
+        auto.driveStraight(1, -30, 0, 0.8);
+        shooter.moveToDefault();
+        sleep(1000);
+        shooter.moveToShoot();
+        sleep(1000);
+        shooter.stop();
+        auto.strafe(0.6, 180, 0.8);
+
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(1000);  // Pause to display last telemetry message.
+
+
+    }
+}
